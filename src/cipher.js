4147ce0a74ebf6854ba431d5ef0b9b731eb3dae0
KarenthCruz <spoonecologic@gmail.com>
2022-07-18 18:11:27 -0500

    Listo, HTML, CSS, JS, Falta Validar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,23 @@
+//Declaración de objeto cipher
 const cipher = {
-  // ...
-};
+  
+  encode: function(offset, string){
+		let stringEncode="";
+		for (let i = 0; i < string.length; i++) {
+			let text = string[i];
+      let ch = String.fromCharCode((text.charCodeAt(0) - 65 + offset) % 26 + 65); stringEncode += ch;
+		}
+		return stringEncode;
+	},
+
+  decode: function(offset, string) {
+		let stringDecode=""
+		for (let i = 0; i < string.length; i++) {
+			let text = string[i];
+      let ch = String.fromCharCode(((text.charCodeAt(0) + 65 - (offset)% 26) %26) + 65); stringDecode += ch;
+		}
+		return stringDecode;
+	}
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const nameUser= document.getElementById("userSend");
+nameUser.addEventListener("click", getName);
+
+function getName(){
+    const inputName= document.getElementById("name");
+    const inputPassword= document.getElementById("password");
+    localStorage.setItem("username", inputName.value);
+    localStorage.setItem("userpassword", inputName.value);
+    inputName.innerHTML= "¡Hola, bienvenida " + inputName.value + "!"
+    inputName.value=""
+    inputPassword.value=""
+}
+
+const btnCoder= document.getElementById("infoCipher");
+btnCoder.addEventListener("click", getEncrypt);
+
+function getEncrypt(){
+    const inputText= document.getElementById("forEncryption");
+    const inputKeyCode= document.getElementById("loockCipher");
+    const textEncryption= cipher.encode(Number(inputKeyCode.value), inputText.value);
+    const inputAnswer= document.getElementById("codeMessage");
+    inputAnswer.value= textEncryption;
+}
+
+const btnDecoder= document.getElementById("infoDecode");
+btnDecoder.addEventListener("click", getDecrypt)
+
+function getDecrypt(){
+    const inpuntEncripted= document.getElementById("forDecode");
+    const inputKeyDecode= document.getElementById("loockCipher2");
+    const textDecripted= cipher.decode(Number(inputKeyDecode.value), inpuntEncripted.value);
+    const inputAnswer2= document.getElementById("textDecode");
+    inputAnswer2.value= textDecripted;
+}
+
+
+
+
+
